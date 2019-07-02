ad3427e80df3a984fa023479c4f40c41ce37d97b
Joe Keegan <jwkeeg@comcast.net>
2019-07-01 19:58:33 -0500

    basic server setup w/o routes

---
+++ server.js
@@ -0,0 +1,43 @@
+// express and mongoose dependencies
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mongoose = require("mongoose");
+
+// scraping tools
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = process.env.PORT || 3000;
+
+// Initialize Express
+var app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Handlebars
+app.engine(
+    "handlebars",
+    exphbs({
+      defaultLayout: "main"
+    })
+  );
+  app.set("view engine", "handlebars");
+
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
+// Routes
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+})
